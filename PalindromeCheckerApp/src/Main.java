b236425d6a16d266e5f3d8214876658cc1c4b5dc
Arjun <aa6709@srmist.edu.in>
2026-03-05 07:09:31 +0530

    Welcome to the App

--- PalindromeCheckerApp/src/Main.java
+++ PalindromeCheckerApp/src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
+class Uc1 {
+    public void show() {
+        System.out.println("Welcome to palindrome checker app");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class PalindromeCheckerApp3 {
+    public static void main(String[] args) {
+        uc1 ob1 = new uc1();
     }
 }
