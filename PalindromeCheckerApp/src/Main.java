dea6850ae764b06e9d0b4457555849ca6abf1500
Arjun <aa6709@srmist.edu.in>
2026-03-05 07:14:16 +0530

    UC2

--- PalindromeCheckerApp/src/Main.java
+++ PalindromeCheckerApp/src/Main.java
@@ -6,9 +6,32 @@ class Uc1 {
         System.out.println("System initialized successfully");
     }
 }
+class Uc2{
+    String name;
+    public boolean find(){
+        boolean palin=true;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the name of the palindrome you want to check: ");
+        name=input.nextLine();
+        for(int i=0;i<name.length()/2;i++){
+            if(name.charAt(i)!=name.charAt(name.length()-i-1)) {
+                palin=false;
+                break;
+            }
+        }
+        if(palin){
+            System.out.println("It is a Palindrome " +name);
+        }
+        else{
+            System.out.println("Not a Palindrome " +name);
+        }
+        return palin;
+    }
+}
 
 public class PalindromeCheckerApp3 {
     public static void main(String[] args) {
-        uc1 ob1 = new uc1();
+        Uc1 ob1 = new Uc1();
+        Uc2 ob2 = new Uc2();
     }
 }
